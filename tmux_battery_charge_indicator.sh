d1c1ecb955348108b93952e50dded8d11e038e41
Paul Jähne <paul.jaehne@gmx.de>
2016-02-09 11:36:18 +0100

    some refactoring
    changed total slots to variable
    changed directory to lowercase
    changed calculation to bash built-in, no bc and awk needed anymore
    changed grep to read from parameter instead of pipeline input via cat

--- tmux_battery_charge_indicator.sh
+++ tmux_battery_charge_indicator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 HEART='♥'
+TOTAL_SLOTS=10
 
 # 1. find battery data
 # for linux
@@ -9,30 +10,30 @@ if [[ $(uname) == 'Linux' ]]; then
 	# check usual locations for battery folder
 	if [[ -d $(echo /sys/class/power_supply/BAT?/ | cut -d " " -f 1) ]]
 	then
-		DIRECTORY=$(echo /sys/class/power_supply/BAT?/ | cut -d " " -f 1)
+		directory=$(echo /sys/class/power_supply/BAT?/ | cut -d " " -f 1)
 	elif [[ -d $(echo /proc/acpi/battery/BAT?/ | cut -d " " -f 1) ]]
 	then
-		DIRECTORY=$(echo /proc/acpi/battery/BAT?/ | cut -d " " -f 1)
+		directory=$(echo /proc/acpi/battery/BAT?/ | cut -d " " -f 1)
 	else
 		exit 1
 	fi
 
 	# check different possibilities for storing the battery data
-	if [[ -f "$DIRECTORY"charge_now ]]
+	if [[ -f "$directory"charge_now ]]
 	then
-		current_charge=$(cat "$DIRECTORY"charge_now)
-		total_charge=$(cat "$DIRECTORY"charge_full)
-		design_charge=$(cat "$DIRECTORY"charge_full_design)
-	elif [[ -f "$DIRECTORY"energy_now ]]
+		current_charge=$(cat "$directory"charge_now)
+		total_charge=$(cat "$directory"charge_full)
+		design_charge=$(cat "$directory"charge_full_design)
+	elif [[ -f "$directory"energy_now ]]
 	then
-		current_charge=$(cat "$DIRECTORY"energy_now)
-		total_charge=$(cat "$DIRECTORY"energy_full)
-		design_charge=$(cat "$DIRECTORY"energy_full_design)
-	elif [[ -f "$DIRECTORY"energy_now ]]
+		current_charge=$(cat "$directory"energy_now)
+		total_charge=$(cat "$directory"energy_full)
+		design_charge=$(cat "$directory"energy_full_design)
+	elif [[ -f "$directory"energy_now ]]
 	then
-		current_charge=$(cat "$DIRECTORY"state | grep 'remaining capacity:' | awk '{print $3}')
-		total_charge=$(cat "$DIRECTORY"info | grep 'last full capacity:' | awk '{print $4}')
-		design_charge=$(cat "$DIRECTORY"BAT1/info | grep 'design capacity:' | awk '{print $3}')
+		current_charge=$(grep "remaining capacity:" "$directory"state | cut -d " " -f 3)
+		total_charge=$(grep "last full capacity:" "$directory"info | cut -d " " -f 4)
+		design_charge=$(grep "design capacity:" "$directory"info | cut -d " " -f 3)
 	else
 		exit 2
 	fi
@@ -40,15 +41,16 @@ if [[ $(uname) == 'Linux' ]]; then
 # for apple (not tested)
 else
 	battery_info=$(ioreg -rc AppleSmartBattery)
-	current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-	total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
-	design_charge=$(echo $battery_info | grep -o '"DesignCapacity" = [0-9]\+' | awk '{print $3}')
+	current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | cut -d " " -f 3)
+	total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | cut -d " " -f 3)
+	design_charge=$(echo $battery_info | grep -o '"DesignCapacity" = [0-9]\+' | cut -d " " -f 3)
 fi
 
 # 2. calcuate slots
-charged_slots=$(echo "$current_charge/$design_charge*10+0.9999" | bc -l | awk -F. '{print $1}')
+# ceil(x/y)=(x+y-1)/y with integer arithmetic
+charged_slots=$((($current_charge*$TOTAL_SLOTS+$design_charge-1)/$design_charge))
 
-dead_slots=$(echo "(1-$total_charge/$design_charge)*10" | bc -l | awk -F. '{print $1}')
+dead_slots=$(($TOTAL_SLOTS-($total_charge*$TOTAL_SLOTS+$design_charge-1)/$design_charge))
 if [[ $dead_slots -lt 1 ]]; then
 	dead_slots=0
 fi
@@ -63,10 +65,10 @@ then
 	done
 fi
 
-if [[ $charged_slots -lt 10 ]]
+if [[ $charged_slots -lt $TOTAL_SLOTS ]]
 then
 	echo -n '#[fg=white]'
-	for i in $(seq 1 $(echo "10-($charged_slots+$dead_slots)" | bc))
+	for i in $(seq 1 $(($TOTAL_SLOTS-($charged_slots+$dead_slots))))
 	do
 		echo -n "$HEART"
 	done
